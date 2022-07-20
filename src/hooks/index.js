f3e823db323a0cd01b5d33847a51e77753baf69a
DinhTranSon <dinh.transon@ddsolution.vn>
2022-07-20 21:04:13 +0700

    init

---
+++ src/hooks/index.js
@@ -0,0 +1,17 @@
+import useResponsive from "./useResponsive";
+import useMobile from "./useMobile";
+import useTablet from "./useTablet";
+import useTabletDown from "./useTabletDown";
+import useLargeTable from "./useLargeTablet";
+import useLargeTableDown from "./useLargeTabletDown";
+import useDesktop from "./useDesktop";
+
+export {
+  useResponsive,
+  useMobile,
+  useTablet,
+  useTabletDown,
+  useLargeTable,
+  useLargeTableDown,
+  useDesktop,
+};
